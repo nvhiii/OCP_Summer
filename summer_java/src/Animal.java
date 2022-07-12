cda5164093e2d896056e84d115124214f53343f5
nvhiii <nahikhan16@gmail.com>
2022-07-12 17:05:40 -0400

    adding classes which I did previously

---
+++ summer_java/src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    String name;
+
+    public String getName() {   // getter method
+        return name;
+    }
+
+    public void setName(String newName) {   // setter method
+        this.name = newName;
+    }
+}
