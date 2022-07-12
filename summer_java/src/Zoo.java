bf8b466937cc1cbc2ab8b07484e9f86e33fdebad
nvhiii <nahikhan16@gmail.com>
2022-07-12 17:08:35 -0400

    testing in terminal stuffs

--- summer_java/src/Zoo.java
+++ summer_java/src/Zoo.java
@@ -2,7 +2,8 @@ public class Zoo {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
 
     }
 }
