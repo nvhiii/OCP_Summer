220ff168dd0e368e302e9e580282fb3653f9f8b4
nvhiii <nahikhan16@gmail.com>
2022-07-13 13:42:05 -0400

    finished with ch1 for online practice, will continue to read in a bit in person

---
+++ summer_java/src/Chick.java
@@ -0,0 +1,14 @@
+public class Chick {
+
+    int height, weight;
+
+    public Chick(int height, int weight) {
+
+        System.out.println("Nice, testing 1, 2, 3...");
+        this.height = height;
+        this.weight = weight;
+
+    }
+
+    // flawless example of a constructor
+}
