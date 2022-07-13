1724940f5948e6dc84b1c1371607f61559505483
nvhiii <nahikhan16@gmail.com>
2022-07-13 10:23:54 -0400

    up to pg 23 notes n stuff

---
+++ summer_java/src/NumberPicker.java
@@ -0,0 +1,13 @@
+import java.util.Random;    // need this import statement in order to use Random class
+
+public class NumberPicker {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+
+    }
+}
+
+// textbooks example of using initial imports
---
+++ summer_java/src/packageB/classB.java
@@ -0,0 +1,17 @@
+package packageB;
+
+import packageA.classA;
+
+public class classB {
+
+    public static void main(String[] args) {
+
+        classA a;
+        System.out.println("Got it");
+
+    }
+
+}
+
+// java was able to tell that I needed to import package A here b/c it is under the same src dir
+
